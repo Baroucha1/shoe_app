44b2a1c7b75468dc467d0be0f55c12ffb5171276
Baroucha1 <97406029+Baroucha1@users.noreply.github.com>
2024-09-17 19:11:03 +0100

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shoe_app/componenets/bottom_nav.dart';
+import 'package:shoe_app/pages/shop_page.dart';
+
+import 'cart_page.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -10,11 +13,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav
+  int _selectedIndex = 0;
+
+  // this methode will update ourselected index
+  void navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
       backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: (){
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(Icons.menu)),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            // logo
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+              ),
+                child: Image.asset(
+              'lib/images/nikelogo.png',
+              color: Colors.white, )
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Divider(
+              color: Colors.grey[800],
+            ),)
+            // other pages
+          ]
+          
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
+
+
 }
