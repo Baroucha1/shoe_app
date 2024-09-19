0a0f29c22a29310352a3eb788539e5ae2632a14a
Baroucha1 <97406029+Baroucha1@users.noreply.github.com>
2024-09-19 13:42:16 +0100

    update
    added shoe class
    update on the shop page

---
+++ lib/componenets/shoe_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shoe_app/models/shoe.dart';
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({
+    super.key, required this.shoe,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          // shoe pic
+          Image.asset(shoe.imagePath),
+
+          // description
+
+          // price + details
+
+          // button to add to cart
+        ],
+      ),
+    );
+  }
+}
+
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../componenets/shoe_tile.dart';
+import '../models/shoe.dart';
+
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
 
@@ -10,13 +13,68 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
+
         // search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search'),
+              Icon(Icons.search)
+            ],
+          ),
+        ),
+
 
         // message
+        Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: Text('Everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+              )
+            ),
+
 
         // hot picks
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Hot picks 🔥',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 24,
+                ),
+              ),
+              Text('See all', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 30,),
+
+        Expanded(child: ListView.builder(
+            itemCount: 4,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index){
+          Shoe shoe1 = Shoe(
+              name: 'blazer',
+              price: '250',
+              imagePath: 'lib/images/blazer1.png',
+              description: 'cool shoe');
+          return ShoeTile(shoe: shoe1,);
+        })
+        ),
+
       ],
     );
   }
